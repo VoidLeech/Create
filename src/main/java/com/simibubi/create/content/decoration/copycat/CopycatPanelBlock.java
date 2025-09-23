ad8817d2d85a4977cd2ba5ee8ee5b39b052e2ab2
TropheusJ <jverrellijr@icloud.com>
2025-09-23 16:31:57 -0400

    whoops forgot to actually finish the commit

--- src/main/java/com/simibubi/create/content/decoration/copycat/CopycatPanelBlock.java
+++ src/main/java/com/simibubi/create/content/decoration/copycat/CopycatPanelBlock.java
@@ -170,11 +170,6 @@ public class CopycatPanelBlock extends WaterloggedCopycatBlock {
 	}
 
 	@Override
-	public boolean skipRendering(BlockState state, BlockState adjacentState, Direction direction) {
-		return false;//state.equals(adjacentState) && direction.getAxis().isHorizontal();
-	}
-
-	@Override
 	public boolean supportsExternalFaceHiding(BlockState state) {
 		return true;
 	}
