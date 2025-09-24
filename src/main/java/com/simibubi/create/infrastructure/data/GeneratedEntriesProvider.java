dc846439f91c6f6bd41310862a0398ac1ba176d5
TropheusJ <jverrellijr@icloud.com>
2025-09-24 02:21:12 -0400

    fix potato cannon ammo

--- src/main/java/com/simibubi/create/infrastructure/data/GeneratedEntriesProvider.java
+++ src/main/java/com/simibubi/create/infrastructure/data/GeneratedEntriesProvider.java
@@ -1,10 +1,8 @@
 package com.simibubi.create.infrastructure.data;
 
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 import com.simibubi.create.AllDamageTypes;
-import com.simibubi.create.Create;
 import com.simibubi.create.api.registry.CreateRegistries;
 import com.simibubi.create.content.equipment.potatoCannon.AllPotatoProjectileTypes;
 import com.simibubi.create.infrastructure.worldgen.AllConfiguredFeatures;
@@ -13,18 +11,26 @@ import com.simibubi.create.infrastructure.worldgen.AllPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.PackOutput;
 
-import io.github.fabricators_of_create.porting_lib.data.DatapackBuiltinEntriesProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
 
-public class GeneratedEntriesProvider extends DatapackBuiltinEntriesProvider {
-	private static final RegistrySetBuilder BUILDER = addBootstraps(new RegistrySetBuilder());
-
-	public GeneratedEntriesProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
-		super(output, registries, BUILDER, Set.of(Create.ID));
+// fabric: reworked to use the fabric API
+public class GeneratedEntriesProvider extends FabricDynamicRegistryProvider {
+	public GeneratedEntriesProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+		super(output, registries);
 	}
 
-	// fabric: this must be reused in the entrypoint, moved to a method
+	@Override
+	protected void configure(HolderLookup.Provider registries, Entries entries) {
+		// make sure all registries below are mirrored here
+		entries.addAll(registries.lookupOrThrow(Registries.DAMAGE_TYPE));
+		entries.addAll(registries.lookupOrThrow(Registries.CONFIGURED_FEATURE));
+		entries.addAll(registries.lookupOrThrow(Registries.PLACED_FEATURE));
+		entries.addAll(registries.lookupOrThrow(CreateRegistries.POTATO_PROJECTILE_TYPE));
+	}
+
+	// fabric: this must be used in the entrypoint, moved to a method
 	public static RegistrySetBuilder addBootstraps(RegistrySetBuilder builder) {
 		return builder.add(Registries.DAMAGE_TYPE, AllDamageTypes::bootstrap)
 				.add(Registries.CONFIGURED_FEATURE, AllConfiguredFeatures::bootstrap)
